4be56a961d92a2068df5ce8c2df18e213d423d55
Jianxin Gao <jianxing@wepay.com>
2018-06-28 17:57:21 -0700

    webhooks stuff

--- src/main/java/com/wepay/resource/WebHooks.java
+++ src/main/java/com/wepay/resource/WebHooks.java
@@ -1,15 +1,23 @@
 package com.wepay.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Response;
 
 @Path("webhooks")
 public class WebHooks {
 
+    private static Logger log = LoggerFactory.getLogger(WebHooks.class);
+
     @POST
     @Path("payments")
-    public Response payments() {
-        return Response.ok().build();
+    public Response payments(final ContainerRequestContext requestContext) {
+        String json = requestContext.getEntityStream().toString();
+        log.info(json);
+        return Response.ok().entity(json).build();
     }
 }
