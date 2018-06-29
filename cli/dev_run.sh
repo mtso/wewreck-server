bccf8d88c1c527a965ec097f6b3c2253644b90f0
Jianxin Gao <jianxing@wepay.com>
2018-06-28 17:17:15 -0700

    hopefully it works

--- cli/dev_run.sh
+++ cli/dev_run.sh
@@ -2,4 +2,4 @@
 this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export "$(cat $this_dir/../config/java_opts)"
 ./gradlew clean installDist
-bash build/install/wehack/bin/wehack server config/wehack.yml 
+bash build/install/wehack/bin/wehack server config/wehack.yml
